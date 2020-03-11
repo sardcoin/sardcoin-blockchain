453af8bd328992f50489ed36d9dd2fdfe93c3378
LivioPompianu <pompianu.livio@gmail.com>
2020-03-11 19:39:24 +0100

    Fixed directory stucture

--- implementation/scripts/api.sh
+++
@@ -1,8 +0,0 @@
-# Avvia il server composer rest con la chiave passata in input
-if [ $# -eq 0 ]
-  then
-    echo "Errore: inserire la chiave per avviare il server rest"
-    exit 1
-fi
-
-composer-rest-server -c admin@sardcoin -n never -y $1 -w true
--- implementation/src/README.md
+++
@@ -1,3 +0,0 @@
-# Smart Contract
-This repository contains the smart contract of the Sardcoin project.
-This is a Hyperledger Fabric smart contract.
